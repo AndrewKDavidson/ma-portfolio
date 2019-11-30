8936552109268f776235404b47bbbf5f90fa9a5e
Andrew <andrew.davidson@sublimemedia.com>
2019-11-30 01:27:28 -0800

    appbar header and main init

---
+++ src/components/mainComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Header from "./headerComponent";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Header />;
+  }
+}
+
+export default Main;
